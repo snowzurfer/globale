afed3aa29a1b76815df4b99adb9d6febc0442e2d
Alberto Taiuti <alberto@albertotaiuti.com>
2023-05-30 05:51:48 -0700

    Add icons to ThreeSceneUI

--- components/ThreeSceneUI.tsx
+++ components/ThreeSceneUI.tsx
@@ -3,6 +3,7 @@ import React, { FunctionComponent, useState } from "react";
 import { SettingsModal } from "./SettingsModal";
 import { useGlobaleStore } from "@/app/store";
 import { ItemsModal } from "./ItemsModal";
+import { Cog6ToothIcon, Squares2X2Icon } from "@heroicons/react/24/outline";
 
 export const ThreeSceneUI: FunctionComponent = () => {
   const modal = useGlobaleStore((state) => state.modal);
@@ -25,16 +26,16 @@ export const ThreeSceneUI: FunctionComponent = () => {
       <div className="fixed top-0 right-0 p-4 flex space-x-4">
         <button
           className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          onClick={() => setModal("settings")}
+          onClick={() => setModal("items")}
         >
-          Settings
+          <div className="flex gap-2"><Squares2X2Icon className="w-5 h-5 "/> <span>Items</span></div>
         </button>
 
         <button
-          className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          onClick={() => setModal("items")}
+            className="px-4 py-2 text-sm font-medium text-white bg-gray-500 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          onClick={() => setModal("settings")}
         >
-          Items
+          <Cog6ToothIcon className="w-5 h-5" />
         </button>
       </div>
 
