74b5d64f25d584a52e2eeccf0901031728443fdb
Alberto Taiuti <alberto@albertotaiuti.com>
2023-06-01 23:48:48 -0700

    Fix pnpm yaml

--- components/SettingsModal.tsx
+++ components/SettingsModal.tsx
@@ -101,7 +101,7 @@ export const SettingsModal: FunctionComponent<{ onClose: () => void }> = ({
               <p>Username: {user.username}</p>
               {/* <p className="text-xs text-gray-400">{user.email}</p> */}
               {user.isAnonymous && <p>(Signed in Anonymously)</p>}
-              {user.isAnonymous && (
+              {/* {user.isAnonymous && (
                 <div className="flex flex-col items-start w-full">
                   <button
                     className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -110,7 +110,7 @@ export const SettingsModal: FunctionComponent<{ onClose: () => void }> = ({
                     Log in with Google
                   </button>
                 </div>
-              )}
+              )} */}
               <button
                 className="w-full px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                 onClick={handleSignOut}
